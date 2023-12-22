f423ec6f164a0ab44e65dcdb05cd0ee61e311853
wiznikvibe <nikhilshetty439@gmail.com>
2023-12-22 19:15:54 +0530

    Folder Structure Added

--- template.py
+++ template.py
@@ -4,40 +4,36 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO)
 
-while True:
-    name = input("Project Master Directory: ")
-    if name != "":
-        break
+project_name = "src"
 
-crucks = [
+list_of_files = [
+    ".github/workflows/.gitkeep",
     f"{project_name}/__init__.py",
-    f"{project_name}/exception.py",
-    f"{project_name}/logger.py",
-    f"{project_name}/utils.py",
     f"{project_name}/components/__init__.py",
-    f"{project_name}/components/data_ingestion.py",
-    f"{project_name}/components/data_validation.py",
-    f"{project_name}/components/data_transformation.py",
-    f"{project_name}/components/model_trainer.py",
-    f"{project_name}/components/model_evaluation.py",
-    f"{project_name}/components/model_pusher.py",
-    f"{project_name}/entity/__init__.py",
-    f"{project_name}/entity/config_entity.py",
-    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/config/configuration.py",
     f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/pipeline/training_pipeline.py",
-    f"{project_name}/pipeline/prediction_pipeline.py",
-    "app.py",
-    "Dockerfile",
-    "setup.py"
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html", 
+    "test.py"
+
+
 ]
 
-for component in crucks:
+for component in list_of_files:
     file_path = Path(component)
     filedir, filename =  os.path.split(file_path)
 
     if filedir != "" :
-        os.makedir(filedir, exist_ok=True)
+        os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory for: {filename}")
 
     if not(os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
